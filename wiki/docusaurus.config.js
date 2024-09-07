b00053905694be5ae8703946c4f9271557085410
Donny F <donny911@hotmail.com>
2024-09-07 09:05:45 -0500

    Change wiki source data

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -14,14 +14,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jlmaners.github.io',
+  url: 'https://dinki.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/View-Assist/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'jlmaners', // Usually your GitHub org/user name.
+  organizationName: 'dinki', // Usually your GitHub org/user name.
   projectName: 'View-Assist', // Usually your repo name.
   deploymentBranch:'gh-pages',
   trailingSlash: false,
